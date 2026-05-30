d3a80ce722325863b6a2948cc6f8ad3765db98ba
onebyteone <floresluera28@gmail.com>
2026-05-29 20:23:16 -0500

    feat: add TestAsteriskSpiralProgram to run AsteriskSpiralProgram

---
+++ 02-java-core-syntax/src/test/java/com/example/TestAsteriskSpiralProgram.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.example.programs.AsteriskSpiralProgram;
+
+public class TestAsteriskSpiralProgram {
+    public static void main(String[] args) {
+        AsteriskSpiralProgram.run();
+    }
+}
